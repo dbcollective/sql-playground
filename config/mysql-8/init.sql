390cf9696d7dd59c1426f81cf3fb8992056f38a8
Al Amin Chayan <6504305+alamin-chayan@users.noreply.github.com>
2022-12-31 20:23:40 +0600

    remove foreign key to handel bulk insert

--- config/mysql-8/init.sql
+++ config/mysql-8/init.sql
@@ -10,14 +10,12 @@ CREATE TABLE IF NOT EXISTS comments (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL,
     message VARCHAR(140) NULL DEFAULT NULL,
-    status ENUM('submitted', 'reviewed', 'published', 'removed') NOT NULL DEFAULT 'submitted',
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    status ENUM('submitted', 'reviewed', 'published', 'removed') NOT NULL DEFAULT 'submitted'
 );
 
 CREATE TABLE IF NOT EXISTS tags (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(25) NULL DEFAULT NULL,
-    UNIQUE (title)
+    title VARCHAR(25) NULL DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS comment_tag (
@@ -30,12 +28,12 @@ CREATE TABLE IF NOT EXISTS comment_tag (
 CREATE TABLE IF NOT EXISTS followers (
     user_id BIGINT NOT NULL,
     follower_id BIGINT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (follower_id) REFERENCES users(id)
+    PRIMARY KEY(user_id, follower_id),
+    INDEX (follower_id)
 );
 
 DELIMITER $$
-CREATE PROCEDURE seeding()
+CREATE PROCEDURE users_seeding()
 BEGIN
     DECLARE i INT DEFAULT 0;
     WHILE i < 1000 DO
@@ -54,30 +52,104 @@ BEGIN
             LIMIT 1000
         )
         SELECT * FROM data;
-
-        -- INSERT INTO comments (user_id, message, status) VALUES (
-        --     i + 1,
-        --     SUBSTRING(MD5(RAND()) FROM 1 FOR 100),
-        --     ELT(0.5 + RAND() * 4, 'submitted', 'reviewed', 'published', 'removed')
-        -- ),
-        -- (
-        --     i + 1,
-        --     SUBSTRING(MD5(RAND()) FROM 1 FOR 100),
-        --     ELT(0.5 + RAND() * 4, 'submitted', 'reviewed', 'published', 'removed')
-        -- );
         SET i = i + 1;
     END WHILE;
-
-    -- DECLARE j INT DEFAULT 0;
-    -- WHILE j < 10000 DO
-    --     INSERT INTO tags (title) VALUES (
-    --         CONCAT('tag', j)
-    --     );
-    --     SET j = j + 1;
-    -- END WHILE;
 END$$
 DELIMITER ;
 
-CALL seeding();
+DELIMITER $$
+CREATE PROCEDURE comments_seeding()
+BEGIN
+    DECLARE i INT DEFAULT 0;
+    WHILE i < 1000 DO
+        INSERT INTO comments (user_id, message, status)
+        WITH RECURSIVE data(x, y, z) AS (
+            SELECT FLOOR((RAND()*10000)+1),
+                SUBSTRING(MD5(RAND()) FROM 1 FOR 100),
+                ELT(0.5 + RAND() * 4, 'submitted', 'reviewed', 'published', 'removed')
+            UNION ALL
+            SELECT FLOOR((RAND()*10000)+1),
+                SUBSTRING(MD5(RAND()) FROM 1 FOR 100),
+                ELT(0.5 + RAND() * 4, 'submitted', 'reviewed', 'published', 'removed')
+            FROM data
+            LIMIT 1000
+        )
+        SELECT * FROM data;
+        SET i = i + 1;
+    END WHILE;
+END$$
+DELIMITER ;
 
-DROP PROCEDURE seeding;
+DELIMITER $$
+CREATE PROCEDURE tag_seeding()
+BEGIN
+    INSERT INTO tags (title)
+    WITH RECURSIVE tmp(x) AS (
+        SELECT 1
+        UNION ALL
+        SELECT x+1 
+        FROM tmp
+        LIMIT 1000
+    ) 
+    SELECT CONCAT('tag', LPAD(x, 4, 0)) FROM tmp;
+
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE followers_seeding()
+BEGIN
+    DECLARE i INT DEFAULT 0;
+    WHILE i < 1000 DO
+        INSERT INTO followers (user_id, follower_id)
+        WITH RECURSIVE data(x, y) AS (
+            SELECT FLOOR((RAND()*10000)+1),
+                FLOOR((RAND()*10000)+1)
+            UNION ALL
+            SELECT FLOOR((RAND()*10000)+1),
+                FLOOR((RAND()*10000)+1)
+            FROM data
+            LIMIT 1000
+        )
+        SELECT * FROM data
+        ON DUPLICATE KEY UPDATE
+        followers.follower_id = VALUES(followers.follower_id);
+        SET i = i + 1;
+    END WHILE;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE comment_tag_seeding()
+BEGIN
+    DECLARE i INT DEFAULT 0;
+    WHILE i < 1000 DO
+        INSERT INTO comment_tag (comment_id, tag_id)
+        WITH RECURSIVE data(x, y) AS (
+            SELECT FLOOR((RAND()*10000)+1),
+                FLOOR((RAND()*100)+1)
+            UNION ALL
+            SELECT FLOOR((RAND()*10000)+1),
+                FLOOR((RAND()*100)+1)
+            FROM data
+            LIMIT 1000
+        )
+        SELECT * FROM data
+        ON DUPLICATE KEY UPDATE
+        comment_tag.tag_id = VALUES(comment_tag.tag_id);
+        SET i = i + 1;
+    END WHILE;
+END$$
+DELIMITER ;
+
+CALL users_seeding();
+CALL comments_seeding();
+CALL tag_seeding();
+CALL followers_seeding();
+CALL comment_tag_seeding();
+
+DROP PROCEDURE users_seeding;
+DROP PROCEDURE comments_seeding;
+DROP PROCEDURE tag_seeding;
+DROP PROCEDURE followers_seeding;
+DROP PROCEDURE comment_tag_seeding;
