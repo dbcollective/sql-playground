8457a122fd993762d2f2eccbda0c9b5ab366ee86
Al Amin Chayan <6504305+alamin-chayan@users.noreply.github.com>
2023-01-03 12:20:53 +0600

    Enable checking for already configured database in MS SQL SERVER

--- config/mssql-2022/entrypoint.sh
+++ config/mssql-2022/entrypoint.sh
@@ -1,6 +1,11 @@
 #wait for the SQL Server to come up
 sleep 30s
 
-echo "running set up script"
-#run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S mssql-2022 -U sa -P $DB_ROOT_PASSWORD -d master -i init.sql
+DB_EXISTS=$(/opt/mssql-tools/bin/sqlcmd -S mssql-2022 -U sa -P $DB_ROOT_PASSWORD -d master -Q "SET NOCOUNT ON; SELECT COUNT(*) FROM sys.databases WHERE name = '$DB_NAME'" | tr -d '-')
+if [ $DB_EXISTS == "1" ];
+then
+    echo "Database is ready to use"
+else
+    echo "Configuring Database and Seeing for the First Time"
+    /opt/mssql-tools/bin/sqlcmd -S mssql-2022 -U sa -P $DB_ROOT_PASSWORD -d master -i init.sql
+fi
